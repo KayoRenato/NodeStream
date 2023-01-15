f2b339d4b8cbda5c877c03e868c3c4bef1de65b4
Kayo.io <kayorenatocontato@gmail.com>
2023-01-14 22:15:24 -0300

    refactor: adjust server directory and create middleware buffer

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -1,8 +1,6 @@
 import http from 'node:http'
 import { Transform } from 'node:stream'
 
-
-
 class InverseNumberStream extends Transform {
     _transform(chunk, encoding, callback) {
         const transformed = Number(chunk.toString()) * -1
@@ -16,15 +14,9 @@ class InverseNumberStream extends Transform {
 // res -> WritableStream
 
 const server = http.createServer(async (req, res) => {
-    const buffer = []
-
-    for await (const chunk of req) {
-        buffer.push(chunk)
-    }
 
     const fullStreamContent = Buffer.concat(buffer).toString()
     console.log(fullStreamContent)
-
 
     return res.end(fullStreamContent)
 
